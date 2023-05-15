aaf6722097c58b29d3090248be3a75c10e2b1694
Lechufane <diego.villafane94@gmail.com>
2023-05-15 11:49:53 -0300

    Se cambio el puerto del proyecto a 8081

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import App from "./app.js";
 import { sequelize } from "./database/database.js";
 
 async function main() {
+  const port = process.env.PORT || 8080;
   try {
     await sequelize.sync({ force: true }); //{ alter: true } para actualizar la base de datos sin borrar los datos
     const script = fs.readFileSync("./database/seed.sql", "utf8"); //leer el sql, y correrlo en un query para rellenar la tabla con los datos.
@@ -15,8 +16,8 @@ async function main() {
     console.error("Unable to connect to the database:", error);
   }
 
-  App.listen(3000, () => {
-    console.log("Express server listening on port 3000");
+  App.listen(port, () => {
+    console.log("Express server listening on port ", port);
   });
 }
 
